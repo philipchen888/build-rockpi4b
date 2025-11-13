a7c5cb0b7b875ef178ae398baae5ea4f31716ca9
philipchen888 <philipchen888@users.noreply.github.com>
2025-11-13 09:41:43 -0800

    use mainline kernel 6.18-rc4, update buildroot

--- linux/mkextlinux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cat >./out/extlinux.conf <<EOF
-label kernel-mainline
-    kernel /`cd ./linux/tmp/boot; ls vmlinuz*`
-    fdt /dtb/rockchip/rk3399-rock-pi-4b.dtb
-    append root=PARTUUID=b921b045-1d rw rootfstype=ext4 init=/sbin/init rootwait
-EOF
